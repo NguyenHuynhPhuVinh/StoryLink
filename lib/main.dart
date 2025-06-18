8e92534cc82d90bbde26a2fad3bba86a47629a35
TomiSakae <tomisakaeap3@gmail.com>
2025-06-18 10:30:22 +0700

    upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Xin Chào App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Ứng dụng Xin Chào')),
+        body: Center(child: Text('xin chào', style: TextStyle(fontSize: 24))),
+      ),
+    );
+  }
+}
